d9bddc3908688db7920f947ba76c26799bca5ab8
Naim <naimtraore@gmail.com>
2017-12-27 21:39:08 -0500

    First commit

---
+++ Flash Chat/CustomMessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomMessageCell.swift
+//  Flash Chat
+//
+//  Created by Naim Youssif Traore.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
